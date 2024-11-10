2d05b494bc7683bfd15dce971e27c2ee858cd913
Adrian Dan <adydan1802@gmail.com>
2024-11-10 22:57:27 +0100

    Second Commit

--- app/api/saveMessage/route.ts
+++ app/api/saveMessage/route.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI || "default_connection_string";
 const options = {};
 
 const client = new MongoClient(uri, options);
